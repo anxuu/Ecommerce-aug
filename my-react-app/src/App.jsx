aa432fec9882c26c538963c11b5d3fdbcff7544c
Ansar bagwan <bansar582@gmail.com>
2025-08-20 03:50:30 +0530

    Added CartPage

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 
 import Homepage from './Pages/Homepage'
+import Cartpage from './Pages/Cartpage'
 
 function App() {
 
   return (
     <>
       <Homepage></Homepage>
+      <br />
+      <Cartpage></Cartpage>
+
+
     </>
   )
 }
